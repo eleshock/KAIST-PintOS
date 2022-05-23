609ad635e48f0cd464390974faadebad01c36acb
Ubuntu <ubuntu@ip-172-31-42-14.ap-northeast-2.compute.internal>
2022-05-23 02:05:22 +0000

    header add

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -152,3 +152,6 @@ int thread_get_load_avg(void);
 void do_iret(struct intr_frame *tf);
 
 #endif /* threads/thread.h */
+
+/*** JACK ***/
+bool cmp_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
