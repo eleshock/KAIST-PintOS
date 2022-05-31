f1e84748dd6b7e06be432f88182aebcbd5f13bda
hyeRexx <96710052+hyeRexx@users.noreply.github.com>
2022-05-31 11:00:22 +0900

    Update syscall.c

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -41,11 +41,10 @@ syscall_init (void) {
 /* The main system call interface */
 void
 syscall_handler (struct intr_frame *f UNUSED) {
-    int64_t intr_case = f->R.rax;
-    
+    int64_t syscall_case = f->R.rax;
     ASSERT(is_user_vaddr(f->rsp)); // rsp 유저 영역에 있는지 확인
     
-	switch (intr_case)
+	switch (syscall_case)
     {
         case SYS_HALT :
             halt();
