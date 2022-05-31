3af2a4ea9a4da8a8fdf35e4f2f8ede15e9d13a7e
Yoonwoo <mryoonwoo@naver.com>
2022-05-31 13:20:16 +0000

    Implemented 'read', 'write', 'tell' syscall function

--- include/lib/kernel/console.h
+++ include/lib/kernel/console.h
@@ -4,5 +4,6 @@
 void console_init (void);
 void console_panic (void);
 void console_print_stats (void);
+void putbuf (const char *buffer, size_t n);         /*** GrilledSalmon ***/
 
 #endif /* lib/kernel/console.h */
--- userprog/syscall.c
+++ userprog/syscall.c
@@ -13,7 +13,10 @@
 #include <filesys/file.h>
 
 /*** GrilledSalmon ***/
-#include "threads/init.h"				
+#include "threads/init.h"	
+#include "userprog/process.h"
+#include "devices/input.h"			// for 'input_getc()'
+#include "kernel/console.h"         // for 'putbuf()'
 
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
@@ -25,6 +28,12 @@ void exit (int status);					/*** GrilledSalmon ***/
 bool create (const char *file, unsigned initial_size);
 bool remove (const char *file);
 int filesize (int fd);
+
+int read (int fd, void *buffer, unsigned size); 	/*** GrilledSalmon ***/
+int write (int fd, void *buffer, unsigned size);    /*** GrilledSalmon ***/
+unsigned tell (int fd);                             /*** GrilledSalmon ***/
+
+static struct lock filesys_lock;                    /*** GrilledSalmon ***/
 
 /* System call.
  *
@@ -40,7 +49,10 @@ int filesize (int fd);
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
 void
-syscall_init (void) {
+syscall_init (void)
+{
+    lock_init(&filesys_lock);       /*** GrilledSalmon ***/
+
 	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
 			((uint64_t)SEL_KCSEG) << 32);
 	write_msr(MSR_LSTAR, (uint64_t) syscall_entry);
@@ -95,15 +107,18 @@ syscall_handler (struct intr_frame *f UNUSED)
             break;
 
         case SYS_READ :
+            f->R.rax = read(f->R.rdi, f->R.rsi, f->R.rdx);
             break;
         
-        case SYS_WRITE : 
+        case SYS_WRITE :
+            f->R.rax = write(f->R.rdi, f->R.rsi, f->R.rdx);
             break;
         
         case SYS_SEEK :
             break;
         
         case SYS_TELL :
+            f->R.rax = tell(f->R.rdi);
             break;
         
         case SYS_CLOSE :
@@ -167,4 +182,76 @@ void exit (int status)
 	/* 자신을 기다리는 부모가 있는 경우 status와 함께 신호 보내줘야 함!! */
 
 	thread_exit();			/* 현재 쓰레드의 상태를 DYING 으로 바꾸고 schedule(다음 쓰레드에게 넘겨줌) */
+}
+
+/*** GrilledSalmon ***/
+int read (int fd, void *buffer, unsigned size)
+{
+    check_address(buffer);
+    
+    uint64_t read_len = 0;              // 읽어낸 길이
+
+	if (fd == 0) { 			            /* fd로 stdin이 들어온 경우 */
+        
+        /*** extra할 때 수정된대유 ***/
+
+        char *buffer_cursor = buffer;
+        lock_acquire(&filesys_lock);    // debugging genie
+        while (read_len < size)
+        {
+            *buffer_cursor++ = input_getc();
+            read_len++;
+        }
+        *buffer_cursor = '\0';
+        lock_release(&filesys_lock);
+
+        return read_len;
+	}
+
+	struct file *now_file = process_get_file(fd);
+
+    if (now_file == NULL || fd == 1){   // fd로 stdout이 들어왔거나 file이 없는 경우
+        return -1;
+    }
+
+    lock_acquire(&filesys_lock);
+    read_len = file_read(now_file, buffer, size);
+    lock_release(&filesys_lock);
+	
+    return read_len;
+}
+
+/*** GrilledSalmon ***/
+int write (int fd, void *buffer, unsigned size)
+{
+    check_address(buffer);
+
+    if (fd == 1) {                      // fd == stdout인 경우
+        lock_acquire(&filesys_lock);
+        putbuf(buffer, size);
+        lock_release(&filesys_lock);
+        return size;
+    }
+
+    struct file *now_file = process_get_file(fd);
+
+    if (now_file == NULL || fd == 1){   // fd로 stdin이 들어왔거나 file이 없는 경우
+        return -1;
+    }
+
+    lock_acquire(&filesys_lock);
+    uint64_t read_len = file_write(now_file, buffer, size);
+    lock_release(&filesys_lock);
+
+    return read_len;
+}
+
+/*** GrilledSalmon ***/
+unsigned tell (int fd)
+{
+    struct file *now_file = process_get_file(fd);
+    if (now_file == NULL) {
+        return -1;
+    }
+    return file_tell(now_file);
 }
