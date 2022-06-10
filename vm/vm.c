3c21613b33f2978172609d6ca30db5cfed20e232
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-06-10 21:58:17 +0900

    conflict resolved

--- vm/vm.c
+++ vm/vm.c
@@ -3,7 +3,6 @@
 #include "threads/malloc.h"
 #include "vm/vm.h"
 #include "vm/inspect.h"
-#include "lib/kernel/hash.h"
 
 /* Initializes the virtual memory subsystem by invoking each subsystem's
  * intialize codes. */
