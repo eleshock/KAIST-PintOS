59606bcd66a76fe7d150dcad0914cfbb1787d327
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-06-14 14:38:38 +0900

    Update thread.h

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -134,7 +134,7 @@ struct thread
 #ifdef VM
 	/* Table for whole virtual memory owned by thread. */
 	struct supplemental_page_table spt;
-	void *if_rsp; // eleshock
+	uintptr_t if_rsp; // eleshock
 #endif
 
 	/* Owned by thread.c. */
